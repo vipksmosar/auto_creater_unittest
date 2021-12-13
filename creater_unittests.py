13a3d9341df6c57bae6261cd2f4ba6fc21d676a1
vipksmosar <admin@npfx.ru>
2021-12-13 22:03:06 +0400

    Update creater_unittests.py
    add_func_read_annotations

--- creater_unittests.py
+++ creater_unittests.py
@@ -15,9 +15,16 @@ class CREATE_UNITTEST_FILE:
             class_setup_string = '\n\n\tdef setUp(self):\n\n\t\t{} = {}()\n'.format(class_obj, class_name)
             class_name_string+=class_setup_string
             for method in self.dict_from_file['class_name'][class_name]['methods']:
-                if not method['def_name'].startswith('_'):
+                if method['def_name'].startswith('__init__'):
                     var = method['def_var'].replace('self,','').replace('self','')
-                    method_string = '\n\n\tdef test_{}(self):\n\t\tself.asertEqual({}.{}({}), return_value)\n\t\tpass\n'.format(method['def_name'], class_obj ,method['def_name'], var)
+                    class_name_string = class_name_string.replace('()','({})'.format(var))
+                elif not method['def_name'].startswith('_'):
+                    var = method['def_var'].replace('self,','').replace('self','')
+                    if method['def_return']==0:
+                        def_return = ''
+                    else:
+                        def_return = ': {}'.format(method['def_return'])
+                    method_string = '\n\n\tdef test_{}(self):\n\t\tself.asertEqual({}.{}({}), return_value {})\n\t\tpass\n'.format(method['def_name'], class_obj ,method['def_name'], var, def_return)
                     class_name_string+=method_string
             body_string+='{}\n\n'.format(class_name_string)
         
@@ -70,25 +77,38 @@ class STRING_PATTERN_FINDER:
         
         if 'def ' in string:
             sub_def = 0
+            type_return = 0
             
             if string.find('#')<string.find('def') and string.find('#')!=-1:
-                return sub_def, 0, 0
+                return sub_def, 0, 0, 0
             
             elif re.search(r'\s+def\s+.+:', string):
+                if re.search(r'def\s+\S+(.*)\s*->\s*\S+:', string):
+                    type_return = re.search('->\s*\S+:',string)[0].replace('->','').replace(' ','').replace(':','')
                 sub_def = 1
                 def_name = re.search(r'\s+def.+?[(]', string)[0].replace('(','').replace(' def ','').replace(' ','')
                 variable_names = re.search(r'[(].+?[)]', string)[0].replace('(','').replace(')','').replace(' ','')
-                return sub_def, def_name, variable_names
+                return sub_def, def_name, variable_names, type_return
             
             elif re.search(r'^def\s+.+:', string):
+                if re.search(r'def\s+\S+(.*)\s*->\s*\S+:', string):
+                    type_return = re.search('->\s*\S+:',string)[0].replace('->','').replace(' ','').replace(':','')                
                 def_name = re.search(r'^def.+?[(]', string)[0][3:].replace('(','').replace(' ','')
                 variable_names = re.search(r'[(].*?[)]', string)[0].replace('(','').replace(')','').replace(' ','')
-                return sub_def, def_name, variable_names
+                return sub_def, def_name, variable_names, type_return
             else:
-                return 0, 0, 0
+                return 0, 0, 0, 0
         else:
-            return 0, 0, 0
-    
+            return 0, 0, 0, 0
+
+    def to_find_qmarks(self, string):
+        if "'''" in string or '"""' in string:
+            if len(re.findall("'''", string))%2==0 and len(re.findall('"""', string))%2==0:
+                return 0
+            else:
+                return 1
+        else:
+            return 0    
     
     def to_find_comment(self, string):
         if '#' in string:
@@ -136,11 +156,15 @@ class PYTHON_FILE_TO_DICT:
     
     def file_to_dict(self):
         class_marker = -1
+        qmarks_result = 0
         regex = STRING_PATTERN_FINDER()
         for string in self.__read_file():
+            qmarks_result+= regex.to_find_qmarks(string)
+            if qmarks_result%2==1:
+                continue
             from_s, import_s = regex.to_find_import_libs(string)
             class_name, parent_class_name = regex.to_find_class(string)
-            sub_def, def_name, def_variables = regex.to_find_defs(string)
+            sub_def, def_name, def_variables, def_return = regex.to_find_defs(string)
             
             if import_s:
                 self.dict_for_file['import_libs'].append({'from':from_s, 'import':import_s})
@@ -150,11 +174,11 @@ class PYTHON_FILE_TO_DICT:
                 self.dict_for_file['class_name'][class_name]={'parent':parent_class_name, 'methods':[]}
 
             elif sub_def:
-                self.dict_for_file['class_name'][class_marker]['methods'].append({'def_name':def_name, 'def_var':def_variables})
+                self.dict_for_file['class_name'][class_marker]['methods'].append({'def_name':def_name, 'def_var':def_variables, 'def_return':def_return})
             
             elif sub_def==0 and def_name!=0:
                 class_marker = -1
-                self.dict_for_file['stand_alone_def_name'].append({'def_name':def_name, 'variable':def_variables})
+                self.dict_for_file['stand_alone_def_name'].append({'def_name':def_name, 'variable':def_variables, 'def_return':def_return})
             
             
         return self.dict_for_file
